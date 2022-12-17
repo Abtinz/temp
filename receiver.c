e68f82205a6545ab5da92e218a1cc6dbf320c263
Abtin Zandi <abtinzandi@gmail.com>
2022-12-17 09:42:58 +0330

    just comment

--- receiver.c
+++ receiver.c
@@ -4,11 +4,15 @@
 #include <sys/shm.h>
 
 int main(){
-    key_t key = ftok("sharedMemoryFile",65);
-    int sahred_memory_id = shmget(key,1024,0666|IPC_CREAT);
-    char *data = (char*) shmat(sahred_memory_id,(void*)0,0);
-    printf("data read from memory:%s\n",data);
+    key_t key = ftok("sharedMemoryFile",65);//shread memory key
+    int sahred_memory_id = shmget(key,1024,0666|IPC_CREAT);//shared memory id
+    char *data = (char*) shmat(sahred_memory_id,(void*)0,0);//data pointer for special shared memory id
+    printf("data read from memory:%s\n",data);//this is receiver so we need to read the data and print it
+    /*
+         detaches from the calling process's address space the shared 
+         memory segment located at the address specified by the argument shmaddr
+    */
     shmdt(data);
-    shmctl(sahred_memory_id,IPC_RMID,NULL);
+    shmctl(sahred_memory_id,IPC_RMID,NULL); //Shared memory segment to be removed and its free!
     return 0;
 }
