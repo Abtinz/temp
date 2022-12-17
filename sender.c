5aadbd60ba877661c053169ad2d47de57a2842fe
Abtin Zandi <abtinzandi@gmail.com>
2022-12-17 09:44:51 +0330

    comment and just comment

--- sender.c
+++ sender.c
@@ -3,13 +3,14 @@
 #include <sys/shm.h>
 
 int main(){
-    key_t key = ftok("sharedMemoryFile",65);
-    int sahred_memory_id = shmget(key,1024,0666|IPC_CREAT);
-    char *data = (char*) shmat(sahred_memory_id,(void*)0,0);
+    
+    key_t key = ftok("sharedMemoryFile",65);//shread memory key
+    int sahred_memory_id = shmget(key,1024,0666|IPC_CREAT);//shared memory id
+    char *data = (char*) shmat(sahred_memory_id,(void*)0,0);//data pointer for special shared memory id
     printf("WRITE DATA:");
     gets(data);
-    printf("data has been written:%s\n",data);
+    printf("data has been written:%s\n",data);//this is sender so we need to write the data and print it
     shmdt(data);
-
     return 0;
+    
 }
